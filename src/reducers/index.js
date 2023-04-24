81883263c94b263d7496830119d253b30a706e04
avindutt <avindutt2369@gmail.com>
2023-04-24 13:48:03 +0530

    Added combineReducers() method

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
-//passing an initial state and action to the function
 import { ADD_MOVIES, ADD_FAVOURITE, REMOVE_FAVOURITE, SET_SHOW_FAVOURITES } from "../actions";
+import { combineReducers } from "redux";
 
+//passing an initial state and action to the function
 const initialMovieState = {
     list: [],
     favourites: [],
     showFavourites: false
 }
-export default function movies (state = initialMovieState, action) {
+export function movies (state = initialMovieState, action) {
     // if(action.type === ADD_MOVIES) {
     //     return {
     //         ...state,
@@ -45,3 +46,31 @@ export default function movies (state = initialMovieState, action) {
 }
 
 //this reducer must return something whether it's a new state or old state
+
+const initialSearchState = {
+    result: {}
+};
+
+export function search (state = initialSearchState, action) {
+    return state;
+}
+
+// creating a root reducer that will contain all other reducers.
+// we can provide only one reducer to our store so all others have to be encapsulated into this one.
+const initialRootState = {
+    movies: initialMovieState,
+    search: initialSearchState
+}
+
+// export default function rootReducer (state = initialRootState, action) {
+//     return {
+//         movies : movies(state.movies, action), // this shows that movies will be managed by movies reducer and search by search reducer
+//         search : search(state.search, action)
+//     }
+// }
+// root reducer will be called everytime when dispatch is called.
+
+export default combineReducers({
+    movies: movies,
+    search: search
+});

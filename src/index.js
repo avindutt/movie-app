2eee677e367c9c2e138dd81dbd0d5fbcf098f68c
avindutt <avindutt2369@gmail.com>
2023-04-24 15:47:48 +0530

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
-import movies from './reducers';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+// function logger(obj, next, action) (here logger is taking 3 arguments and below is the currying function for it)
+// logger(obj)(next)(action)
+const logger = function ({dispatch, getState}) {
+  return function (next) { 
+    return function (action) {
+      // middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action); // calling the next middleware if any. If there's not any then instead of next dispatch function will be called and action will be sent to reducer.
+    }
+  }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log(store);
 // console.log('Before state', store.getState());
 
